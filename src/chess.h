03cbbbdf78c542a42b64fd33e2198aa44c9f8ded
Katerina <kosareva.katerina2016@ya.ru>
2020-05-08 16:06:18 +0700

    Create header file

---
+++ src/chess.h
@@ -0,0 +1,11 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <iostream>
+using namespace std;
+
+void print(char chess[][9]);
+void proverka(string xod, int m[2][2]);
+void Move(char chess[][9], int m[2][2]);
+
+#endif
